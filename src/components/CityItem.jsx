0a4b7ad9590a6088dd5079678dcb037ee585553b
zahnx <breadsw01@gmail.com>
2025-03-28 15:05:17 +0800

    Refactored the code and used useReducer for State Management

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -12,7 +12,13 @@ const formatDate = (date) =>
 function CityItem({ city }) {
   // console.log(city);
   const { cityName, emoji, date, id, position } = city;
-  const { currentCity } = useCities();
+  const { currentCity, deleteCity } = useCities();
+
+  function handleClick(e) {
+    e.preventDefault();
+    console.log("Test");
+    deleteCity(id);
+  }
 
   return (
     <li>
@@ -25,7 +31,9 @@ function CityItem({ city }) {
         <span className={styles.emoji}>{emoji}</span>
         <h3 className={styles.name}>{cityName}</h3>
         <time className={styles.date}>{formatDate(date)}</time>
-        <button className={styles.deleteBtn}>&times;</button>
+        <button className={styles.deleteBtn} onClick={handleClick}>
+          &times;
+        </button>
       </Link>
     </li>
   );
